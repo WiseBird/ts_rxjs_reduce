9020f89f939b2640c6608d97b4914a2b95ff14fe
sergey.sokolov <sergey.sokolov@behavox.com>
2018-02-21 18:31:42 +0300

    Next commit

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,25 @@
-interface Observable<T> {
-    reduce(accumulator: (acc: T[], value: T) => T[], seed: T[]): Observable<T[]>;
-    reduce(accumulator: (acc: T, value: T) => T, seed?: T): Observable<T>;
-    reduce<R>(accumulator: (acc: R, value: T) => R, seed: R): Observable<R>;
-}
+// interface Observable<T> {
+//     reduce(accumulator: (acc: T[], value: T) => T[], seed: T[]): Observable<T[]>;
+//     reduce(accumulator: (acc: T, value: T) => T, seed?: T): Observable<T>;
+//     reduce<R>(accumulator: (acc: R, value: T) => R, seed: R): Observable<R>;
+// }
 
-// import {Observable} from 'rxjs';
+import {Observable} from 'rxjs';
 
 (null as Observable<{attribute: string, values: string[]}>)
     .reduce((acc, val) => ({
         ...acc,
         [val.attribute]: val.values,
     }), {});
+
+
+// import {Observable} from 'rxjs/Rx';
+// import {reduce} from 'rxjs/operators';
+//
+// (null as Observable<{attribute: string, values: string[]}>)
+//     .let(
+//         reduce((acc, val) => ({
+//             ...acc,
+//             [val.attribute]: val.values,
+//         }), {}),
+//     );
